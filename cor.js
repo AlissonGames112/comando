8aacb66a9a998944eb09331bd72f4ba7fa6adfe7
AlissonGames112 <37303237+AlissonGames112@users.noreply.github.com>
2018-09-25 18:24:57 -0300

    Create cor.js

---
+++ cor.js
@@ -0,0 +1,222 @@
+exports.run = (client, message, args) => {
+    if (message.channel.id == client.guilds.get("399756948951662592").channels.get("403664026703233024").id) {
+    if (message.content.startsWith("ad!cor preto")) {
+        message.guild.members.get(message.author.id).addRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor black")) {
+        message.guild.members.get(message.author.id).addRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor azul")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).addRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor blue")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).addRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor rosa")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).addRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor pink")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).addRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor vermelho")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).addRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor red")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).addRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor ciano")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).addRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor cyano")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).addRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor roxo")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).addRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor purple")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).addRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor verde")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).addRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor green")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).addRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor amarelo")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).addRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor yellow")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).addRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).removeRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor laranja")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).addRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+    if (message.content.startsWith("ad!cor orange")) {
+        message.guild.members.get(message.author.id).removeRole("403662081762656257"); //preto
+        message.guild.members.get(message.author.id).removeRole("403662254224179200"); //azul
+        message.guild.members.get(message.author.id).removeRole("403661565766664195"); //rosa
+        message.guild.members.get(message.author.id).removeRole("403662715253555201"); //vermelho
+        message.guild.members.get(message.author.id).removeRole("403661849989742592"); //ciano
+        message.guild.members.get(message.author.id).removeRole("403661662659280896"); //roxo
+        message.guild.members.get(message.author.id).removeRole("403661728099074063"); //verde
+        message.guild.members.get(message.author.id).removeRole("403262655067979783"); //amarelo
+        message.guild.members.get(message.author.id).addRole("403662329000230922"); //laranja
+        message.reply("**Cor setada! :star2:**");
+    }
+  } else {
+      message.reply("**Aqui não é lugar de trocar a cor!**")
+  }
+}
